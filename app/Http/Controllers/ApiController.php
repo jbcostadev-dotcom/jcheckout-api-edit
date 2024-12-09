d945f3c85532e53bc7166608740b401b8f2a88e9
joyanbarai <baraijoyan@gmail.com>
2024-12-09 21:47:24 +0600

    No message

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use DB;
-use App\Models\Sanctum\PersonalAccessToken;
 use App\Models\Sanctum\NewAccessToken;
-use App\Models\User;
+use App\Models\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Sanctum\Sanctum;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
@@ -26,18 +24,18 @@ class ApiController extends Controller
             $coresTema = DB::select(DB::raw("
                 SELECT cor_tema as cor FROM usuario_preferencias WHERE id_usuario = :id_usuario
             "), ['id_usuario' => $idusuario->id_usuario]);
-            
+
 
             $sqlVerificaToken = "
                 SELECT dt_fim_token
                 FROM usuario_" . $idusuario->tipo_usuario . "
-                WHERE id_usuario_" . $idusuario->tipo_usuario . " = " . $idusuario->id_usuario . " 
+                WHERE id_usuario_" . $idusuario->tipo_usuario . " = " . $idusuario->id_usuario . "
             ";
 
             $verificaToken = DB::select(DB::raw($sqlVerificaToken));
             $fimToken = $verificaToken[0]->dt_fim_token;
 
-            if(date('Y-m-d H:i:s') > $fimToken){
+            if (date('Y-m-d H:i:s') > $fimToken) {
                 return response()->json([
                     'status' => 400,
                     'mensagem' => 'Token vencido.'
@@ -64,11 +62,53 @@ class ApiController extends Controller
         }
     }
 
+    public function inscreve(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'usuario' => 'bail|required|string|max:50|unique:users,username',
+            'senha' => 'bail|required|string|min:8|max:50',
+        ], [
+            'usuario.required' => 'Nome de usuário é obrigatório!',
+            'usuario.max' => 'O nome de usuário não pode ter mais de 50 caracteres',
+            'usuario.unique' => 'O nome de usuário deve ser único',
+            'senha.required' => 'Senha é necessária!',
+            'senha.min' => 'A senha deve ter pelo menos 8 caracteres!',
+            'senha.max' => 'A senha não pode ter mais de 50 caracteres!',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $parentUserId = DB::table('usuario_pai')->insertGetId([
+            'usuario' => $request->usuario,
+            'senha' => Hash::make($request->senha),
+            'dt_inicio_token' => now()->format('Y-m-d'),
+            'dt_fim_token' => now()->addMonth()->format('Y-m-d'),
+            'qtd_sub_usuarios' => 1,
+            'qtd_lojas' => 1,
+            'token' => Hash::make($request->senha),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => $request->usuario,
+            'username' => $request->usuario,
+            'password' => Hash::make($request->senha),
+            'tipo_usuario' => 'pai',
+            'id_usuario' => $parentUserId,
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário criado com sucesso',
+            'status' => 201
+        ], 201);
+    }
+
     public function getStatusToken(Request $request)
     {
         $query = DB::select(
             DB::raw(
-                "SELECT dt_inicio_token as inicio, dt_fim_token as fim 
+                "SELECT dt_inicio_token as inicio, dt_fim_token as fim
             FROM usuario_pai
             WHERE token = :token"
             ),
--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\LojaController;
-use App\Http\Controllers\ConfiguracoesController;
 use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\CheckoutController;
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ConfiguracoesController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PHPMailerController;
-use App\Http\Controllers\WhatsappController;
 use App\Http\Controllers\EmailController;
+use App\Http\Controllers\LojaController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\WhatsappController;
+use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('inscreve', [ApiController::class, 'inscreve']);
 
 Route::get('/testebc', [CheckoutController::class, 'getQrCodeBc']);
 
@@ -71,115 +60,110 @@ Route::post('/checkout/updateDados', [CheckoutController::class, 'updateDados'])
 //Métods p/ visitas!
 Route::post('/localcliente', [CarrinhoController::class, 'localCliente']);
 
-Route::group(['middleware' => ['auth:sanctum', 'cors']], function() {
-   Route::get('/verificaToken', [ApiController::class, 'verificaTokenApi']);
-   Route::post('/layoutsLoja', [LojaController::class, 'getLayoutsLoja']);
-   //Métodos p/ lojas! :D
-   Route::post('/adicionaLoja', [LojaController::class, 'adicionaLoja'])->name('adicionaloja');
-   Route::post('/layoutsLoja', [LojaController::class, 'getLayoutsLoja']);
-   Route::post('/getLojas', [LojaController::class, 'getLojas']);
-   Route::post('/updateLoja', [LojaController::class, 'updateLoja']);
-   Route::post('/getProdutos', [LojaController::class, 'getProdutos']);
-   Route::post('/updateProduto', [LojaController::class, 'updateProduto']);
-   Route::post('/deleteProduto', [LojaController::class, 'deleteProduto']);
-   Route::post('/importarCsv', [LojaController::class, 'importarCsv']);
-   Route::post('/updateSnLoja', [LojaController::class, 'updateSnLoja']);
-   Route::post('/getProdutosLoja', [LojaController::class, 'getProdutosLoja']);
-   Route::post('/adicionaCategoria', [LojaController::class, 'adicionaCategoria']);
-   Route::post('/getCategorias', [LojaController::class, 'getCategorias']);
-   Route::post('/updateCategoriaProduto', [LojaController::class, 'updateCategoriaProduto']);
-   Route::post('/deleteCategoria', [LojaController::class, 'deleteCategoria']);
-   Route::post('/dashboard/adicionarProdutoManual', [DashboardController::class, 'adicionarProdutoManual']);
-   
-   
-   //Métodos p/ configurações! :D
-   Route::post('/adicionarDominio', [ConfiguracoesController::class, 'adicionarDominio'])->middleware(\Fruitcake\Cors\HandleCors::class);
-   Route::post('/dominio/getLojas', [ConfiguracoesController::class, 'getLojas']);
-   Route::post('/getDominios', [ConfiguracoesController::class, 'getDominios']);
-   Route::post('/getLogDominio', [ConfiguracoesController::class, 'getLogDominio']);
-   Route::post('/dominio/log/{tipo_log}', [ConfiguracoesController::class, 'updateLogDominio']);
-   Route::post('/dominio/apagarDominio', [ConfiguracoesController::class, 'apagarDominio']);
-   
-   
-   //Métodos p/ Aba checkout! :D
-   Route::post('/dashboard/getLojasCheckout', [LojaController::class, 'getLojasCheckout']);
-   Route::post('/dashboard/updateFreteLoja', [LojaController::class, 'updateFreteLoja']);
-   Route::post('/dashboard/getFretesLoja', [LojaController::class, 'getFretesLoja']);
-   
-   
-   
-   //Métodos p/ usuário
-   Route::post('/usuario/updateTema', [UsuarioController::class, 'updateTema']);
-   
-   
-   //Métodos p/ dashboard (novo)
-   
-   Route::post('/dashboard/updateChavePix', [DashboardController:: class, 'updateChavePix']);
-   Route::post('/dashboard/getDadosPagamento', [DashboardController:: class, 'getDadosPagamento']);
-   Route::post('/getUsuariosOnline', [DashboardController:: class, 'getUsuariosOnline']);
-   Route::post('/dashboard/getCards', [DashboardController:: class, 'getCards']);
-   Route::post('/dashboard/adicionarBannerLoja', [DashboardController:: class, 'adicionarBannerLoja']);
-   Route::post('/dashboard/salvaVariacao', [DashboardController:: class, 'salvaVariacao']);
-   Route::post('/dashboard/getCardsPerfil', [DashboardController:: class, 'getCardsPerfil']);
-   Route::post('/dashboard/apagaFrete', [DashboardController:: class, 'apagaFrete']);
-   Route::post('/dashboard/salvaPixelFb', [DashboardController:: class, 'salvarPixelFb']);
-   Route::post('/dashboard/getProdutosVariacao', [DashboardController:: class, 'getProdutosVariacao']);
-   Route::post('/dashboard/salvaVariacaoNovo', [DashboardController:: class, 'salvaVariacaoNovo']);
-   Route::post('/dashboard/updatePreferencias', [DashboardController:: class, 'updatePreferencias']);
-   Route::post('/dashboard/getPreferencias', [DashboardController:: class, 'getPreferencias']);
-   Route::post('/dashboard/getDominioCheckout', [DashboardController:: class, 'getDominioCheckout']);
-   Route::post('/dashboard/getOrderBumpProduto', [DashboardController:: class, 'getOrderBumpProduto']);
-   Route::post('/dashboard/updateOrderBump', [DashboardController:: class, 'updateOrderBump']);
-   Route::post('/dashboard/resetaEst', [DashboardController:: class, 'resetaEst']);
-   Route::post('/dashboard/updateFretePadrao', [DashboardController:: class, 'updateFretePadrao']);
-   Route::post('/dashboard/getFretePadrao', [DashboardController:: class, 'getFretePadrao']);
-   Route::post('/dashboard/deleteLoja', [DashboardController:: class, 'deleteLoja']);
-   Route::post('/dashboard/getSuitpay', [DashboardController:: class, 'getSuitpay']);
-   Route::post('/dashboard/updateSuitpay', [DashboardController:: class, 'updateSuitpay']);
-   Route::post('/dashboard/getWhatsapp', [DashboardController:: class, 'getWhatsapp']);
-   Route::post('/dashboard/updateWhatsapp', [DashboardController:: class, 'updateWhatsapp']);
-   Route::post('/dashboard/verificaWhatsapp', [DashboardController:: class, 'verificaWhatsapp']);
-   Route::post('/dashboard/getStatusWhatsapp', [DashboardController:: class, 'getStatusWhatsapp']);
-   Route::post('/dashboard/enviaWhats', [DashboardController:: class, 'enviaWhats']);
-   Route::post('/dashboard/getBc', [DashboardController:: class, 'getBc']);
-   Route::post('/dashboard/updateBc', [DashboardController:: class, 'updateBc']);
-   Route::post('/dashboard/getLojasEmail', [DashboardController:: class, 'getLojasEmail']);
-   Route::post('/dashboard/getSmtpLoja', [DashboardController:: class, 'getSmtpLoja']);
-   Route::post('/dashboard/verificaSmtp', [EmailController:: class, 'verificaSmtp']);
-   Route::post('/dashboard/updateSmtp', [DashboardController:: class, 'updateSmtp']);
-   Route::post('/dashboard/deleteSuitpay', [DashboardController:: class, 'deleteSuitpay']);
-   Route::post('/dashboard/verificaEmailPedido', [DashboardController:: class, 'verificaEmailPedido']);
-   Route::post('/dashboard/enviaConfirmacaoPedido', [DashboardController:: class, 'enviaConfirmacaoPedido']);
-   Route::post('/dashboard/enviaLembretePagamento', [DashboardController:: class, 'enviaLembretePagamento']);
-   Route::post('/dashboard/salvaCopiaCola', [DashboardController:: class, 'salvaCopiaCola']);
-   Route::post('/dashboard/getCountCodigos', [DashboardController:: class, 'getCountCodigos']);
-   Route::post('/dashboard/deleteCopiaCola', [DashboardController:: class, 'deleteCopiaCola']);
-   Route::post('/dashboard/getCartaoLoja', [DashboardController:: class, 'getCartaoLoja']);
-   Route::post('/dashboard/ativaCartaoLoja', [DashboardController:: class, 'ativaCartaoLoja']);
-   Route::post('/dashboard/ativaVbvLoja', [DashboardController:: class, 'ativaVbvLoja']);
-   Route::post('/dashboard/updateMsgCartao', [DashboardController:: class, 'updateMsgCartao']);
-   Route::post('/dashboard/getCartoes', [DashboardController:: class, 'getCartoes']);
-   Route::post('/dashboard/deleteInfo', [DashboardController:: class, 'deleteInfo']);
-   Route::post('/dashboard/salvaPixelTaboola', [DashboardController:: class, 'salvaPixelTaboola']);
-   Route::post('/dashboard/updateDominioPadrao', [DashboardController:: class, 'updateDominioPadrao']);
-   Route::post('/dashboard/getBins', [DashboardController:: class, 'getBins']);
-   Route::post('/dashboard/updateBinsUser', [DashboardController:: class, 'updateBinsUser']);
-   Route::post('/dashboard/getFacebooks', [DashboardController:: class, 'getFacebooks']);
-
-   
-   //Métodos p/ aba pedidos!
-   Route::post('/dashboard/getPedidos', [DashboardController::class, 'getPedidos']);
-   Route::post('/dashboard/deletapedido', [DashboardController::class, 'deletaPedido']);
-   
-   
-   //Métodos Shopify
-   Route::post('/dashboard/getShopifyLoja', [DashboardController::class, 'getShopifyLoja']);
-   Route::post('/dashboard/updateShopifyLoja', [DashboardController::class, 'updateShopifyLoja']);
-   Route::post('/dashboard/getDadosShopify', [DashboardController::class, 'getDadosShopify']);
-   Route::post('/dashboard/updatePreferenciaShopify', [DashboardController::class, 'updatePreferenciaShopify']);
-   Route::post('/dashboard/desativaShopify', [DashboardController::class, 'desativaShopify']);
-
-   
+Route::group(['middleware' => ['auth:sanctum', 'cors']], function () {
+    Route::get('/verificaToken', [ApiController::class, 'verificaTokenApi']);
+    Route::post('/layoutsLoja', [LojaController::class, 'getLayoutsLoja']);
+    //Métodos p/ lojas! :D
+    Route::post('/adicionaLoja', [LojaController::class, 'adicionaLoja'])->name('adicionaloja');
+    Route::post('/layoutsLoja', [LojaController::class, 'getLayoutsLoja']);
+    Route::post('/getLojas', [LojaController::class, 'getLojas']);
+    Route::post('/updateLoja', [LojaController::class, 'updateLoja']);
+    Route::post('/getProdutos', [LojaController::class, 'getProdutos']);
+    Route::post('/updateProduto', [LojaController::class, 'updateProduto']);
+    Route::post('/deleteProduto', [LojaController::class, 'deleteProduto']);
+    Route::post('/importarCsv', [LojaController::class, 'importarCsv']);
+    Route::post('/updateSnLoja', [LojaController::class, 'updateSnLoja']);
+    Route::post('/getProdutosLoja', [LojaController::class, 'getProdutosLoja']);
+    Route::post('/adicionaCategoria', [LojaController::class, 'adicionaCategoria']);
+    Route::post('/getCategorias', [LojaController::class, 'getCategorias']);
+    Route::post('/updateCategoriaProduto', [LojaController::class, 'updateCategoriaProduto']);
+    Route::post('/deleteCategoria', [LojaController::class, 'deleteCategoria']);
+    Route::post('/dashboard/adicionarProdutoManual', [DashboardController::class, 'adicionarProdutoManual']);
 
 
+    //Métodos p/ configurações! :D
+    Route::post('/adicionarDominio', [ConfiguracoesController::class, 'adicionarDominio'])->middleware(\Fruitcake\Cors\HandleCors::class);
+    Route::post('/dominio/getLojas', [ConfiguracoesController::class, 'getLojas']);
+    Route::post('/getDominios', [ConfiguracoesController::class, 'getDominios']);
+    Route::post('/getLogDominio', [ConfiguracoesController::class, 'getLogDominio']);
+    Route::post('/dominio/log/{tipo_log}', [ConfiguracoesController::class, 'updateLogDominio']);
+    Route::post('/dominio/apagarDominio', [ConfiguracoesController::class, 'apagarDominio']);
+
+
+    //Métodos p/ Aba checkout! :D
+    Route::post('/dashboard/getLojasCheckout', [LojaController::class, 'getLojasCheckout']);
+    Route::post('/dashboard/updateFreteLoja', [LojaController::class, 'updateFreteLoja']);
+    Route::post('/dashboard/getFretesLoja', [LojaController::class, 'getFretesLoja']);
+
+
+    //Métodos p/ usuário
+    Route::post('/usuario/updateTema', [UsuarioController::class, 'updateTema']);
+
+
+    //Métodos p/ dashboard (novo)
+
+    Route::post('/dashboard/updateChavePix', [DashboardController:: class, 'updateChavePix']);
+    Route::post('/dashboard/getDadosPagamento', [DashboardController:: class, 'getDadosPagamento']);
+    Route::post('/getUsuariosOnline', [DashboardController:: class, 'getUsuariosOnline']);
+    Route::post('/dashboard/getCards', [DashboardController:: class, 'getCards']);
+    Route::post('/dashboard/adicionarBannerLoja', [DashboardController:: class, 'adicionarBannerLoja']);
+    Route::post('/dashboard/salvaVariacao', [DashboardController:: class, 'salvaVariacao']);
+    Route::post('/dashboard/getCardsPerfil', [DashboardController:: class, 'getCardsPerfil']);
+    Route::post('/dashboard/apagaFrete', [DashboardController:: class, 'apagaFrete']);
+    Route::post('/dashboard/salvaPixelFb', [DashboardController:: class, 'salvarPixelFb']);
+    Route::post('/dashboard/getProdutosVariacao', [DashboardController:: class, 'getProdutosVariacao']);
+    Route::post('/dashboard/salvaVariacaoNovo', [DashboardController:: class, 'salvaVariacaoNovo']);
+    Route::post('/dashboard/updatePreferencias', [DashboardController:: class, 'updatePreferencias']);
+    Route::post('/dashboard/getPreferencias', [DashboardController:: class, 'getPreferencias']);
+    Route::post('/dashboard/getDominioCheckout', [DashboardController:: class, 'getDominioCheckout']);
+    Route::post('/dashboard/getOrderBumpProduto', [DashboardController:: class, 'getOrderBumpProduto']);
+    Route::post('/dashboard/updateOrderBump', [DashboardController:: class, 'updateOrderBump']);
+    Route::post('/dashboard/resetaEst', [DashboardController:: class, 'resetaEst']);
+    Route::post('/dashboard/updateFretePadrao', [DashboardController:: class, 'updateFretePadrao']);
+    Route::post('/dashboard/getFretePadrao', [DashboardController:: class, 'getFretePadrao']);
+    Route::post('/dashboard/deleteLoja', [DashboardController:: class, 'deleteLoja']);
+    Route::post('/dashboard/getSuitpay', [DashboardController:: class, 'getSuitpay']);
+    Route::post('/dashboard/updateSuitpay', [DashboardController:: class, 'updateSuitpay']);
+    Route::post('/dashboard/getWhatsapp', [DashboardController:: class, 'getWhatsapp']);
+    Route::post('/dashboard/updateWhatsapp', [DashboardController:: class, 'updateWhatsapp']);
+    Route::post('/dashboard/verificaWhatsapp', [DashboardController:: class, 'verificaWhatsapp']);
+    Route::post('/dashboard/getStatusWhatsapp', [DashboardController:: class, 'getStatusWhatsapp']);
+    Route::post('/dashboard/enviaWhats', [DashboardController:: class, 'enviaWhats']);
+    Route::post('/dashboard/getBc', [DashboardController:: class, 'getBc']);
+    Route::post('/dashboard/updateBc', [DashboardController:: class, 'updateBc']);
+    Route::post('/dashboard/getLojasEmail', [DashboardController:: class, 'getLojasEmail']);
+    Route::post('/dashboard/getSmtpLoja', [DashboardController:: class, 'getSmtpLoja']);
+    Route::post('/dashboard/verificaSmtp', [EmailController:: class, 'verificaSmtp']);
+    Route::post('/dashboard/updateSmtp', [DashboardController:: class, 'updateSmtp']);
+    Route::post('/dashboard/deleteSuitpay', [DashboardController:: class, 'deleteSuitpay']);
+    Route::post('/dashboard/verificaEmailPedido', [DashboardController:: class, 'verificaEmailPedido']);
+    Route::post('/dashboard/enviaConfirmacaoPedido', [DashboardController:: class, 'enviaConfirmacaoPedido']);
+    Route::post('/dashboard/enviaLembretePagamento', [DashboardController:: class, 'enviaLembretePagamento']);
+    Route::post('/dashboard/salvaCopiaCola', [DashboardController:: class, 'salvaCopiaCola']);
+    Route::post('/dashboard/getCountCodigos', [DashboardController:: class, 'getCountCodigos']);
+    Route::post('/dashboard/deleteCopiaCola', [DashboardController:: class, 'deleteCopiaCola']);
+    Route::post('/dashboard/getCartaoLoja', [DashboardController:: class, 'getCartaoLoja']);
+    Route::post('/dashboard/ativaCartaoLoja', [DashboardController:: class, 'ativaCartaoLoja']);
+    Route::post('/dashboard/ativaVbvLoja', [DashboardController:: class, 'ativaVbvLoja']);
+    Route::post('/dashboard/updateMsgCartao', [DashboardController:: class, 'updateMsgCartao']);
+    Route::post('/dashboard/getCartoes', [DashboardController:: class, 'getCartoes']);
+    Route::post('/dashboard/deleteInfo', [DashboardController:: class, 'deleteInfo']);
+    Route::post('/dashboard/salvaPixelTaboola', [DashboardController:: class, 'salvaPixelTaboola']);
+    Route::post('/dashboard/updateDominioPadrao', [DashboardController:: class, 'updateDominioPadrao']);
+    Route::post('/dashboard/getBins', [DashboardController:: class, 'getBins']);
+    Route::post('/dashboard/updateBinsUser', [DashboardController:: class, 'updateBinsUser']);
+    Route::post('/dashboard/getFacebooks', [DashboardController:: class, 'getFacebooks']);
+
+
+    //Métodos p/ aba pedidos!
+    Route::post('/dashboard/getPedidos', [DashboardController::class, 'getPedidos']);
+    Route::post('/dashboard/deletapedido', [DashboardController::class, 'deletaPedido']);
+
+
+    //Métodos Shopify
+    Route::post('/dashboard/getShopifyLoja', [DashboardController::class, 'getShopifyLoja']);
+    Route::post('/dashboard/updateShopifyLoja', [DashboardController::class, 'updateShopifyLoja']);
+    Route::post('/dashboard/getDadosShopify', [DashboardController::class, 'getDadosShopify']);
+    Route::post('/dashboard/updatePreferenciaShopify', [DashboardController::class, 'updatePreferenciaShopify']);
+    Route::post('/dashboard/desativaShopify', [DashboardController::class, 'desativaShopify']);
 });
